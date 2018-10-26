0bd8367aea3923b176b462b1e79b091833057f31
udaypathania31 <42261208+udaypathania31@users.noreply.github.com>
2018-10-26 10:30:29 +0530

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+ class q2
+ {
+
+	public static void main(String[] args) 
+     {
+     ArrayList<String> s=new ArrayList<String>();
+     s.add("Ayushi Kapoor");
+     s.add("Siddharth Monga");
+     s.add("Riya");
+     Collections.sort(s);
+     System.out.println(s);
+
+     ArrayList<Integer> i=new ArrayList<Integer>();
+
+     i.add(10);
+     i.add(200);
+     i.add(3);
+     Collections.sort(i);
+     System.out.println(i);
+	}
+
+}
